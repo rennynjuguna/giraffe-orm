79e3990a9d1005b3986a144e7c18746f3643c51b
Derek Kambo <Tnguyen410@student.gsu.edu>
2023-02-21 11:29:41 +0300

    Added connection to sqlite db

--- giraffes.rb
+++ giraffes.rb
@@ -1,7 +1,9 @@
+require 'sqlite3'
+
 class Giraffe
   attr_accessor :name, :age, :height, :weight, :gender
 
-  @@all = []
+  @@db = SQLite3::Database.new('giraffes.db')
 
   def initialize(name, age, height, weight, gender)
     @name = name
@@ -9,30 +11,43 @@ class Giraffe
     @height = height
     @weight = weight
     @gender = gender
-    @@all << self
   end
 
   def self.all
-    @@all
+    rows = @@db.execute("SELECT * FROM giraffes")
+    rows.map do |row|
+      Giraffe.new(row[0], row[1], row[2], row[3], row[4])
+    end
   end
 
-  def self.new_giraffe(name, age, height, weight, gender)
-    @@all << Giraffe.new(name, age, height, weight, gender)
+  def save
+    @@db.execute("INSERT INTO giraffes (name, age, height, weight, gender) VALUES (?, ?, ?, ?, ?)",
+                 @name, @age, @height, @weight, @gender)
+  end
+
+  def self.create(name, age, height, weight, gender)
+    giraffe = Giraffe.new(name, age, height, weight, gender)
+    giraffe.save
+    giraffe
+  end
+
+  def self.db
+    @@db
   end
 end
 
-# the objects
-g1 = Giraffe.new("George", 3, 12, 800, "male")
-g2 = Giraffe.new("Gina", 4, 14, 1000, "female")
-g3 = Giraffe.new("Greg", 5, 16, 1200, "male")
-g4 = Giraffe.new_giraffe("Renny", 6, 10, 1600, "male")
-g5 = Giraffe.new_giraffe("Alice", 10, 18, 1400, "female")
+# create the giraffes table
+Giraffe.db.execute("CREATE TABLE IF NOT EXISTS giraffes (name TEXT PRIMARY KEY, age INTEGER, height INTEGER, weight INTEGER, gender TEXT)")
 
-pp Giraffe.all
+# test the Giraffe class
 
-=begin
-# @@all contents
-Giraffe.all.each do |giraffe|
-  puts "Name: #{giraffe.name}, Age: #{giraffe.age}, Height: #{giraffe.height}, Weight: #{giraffe.weight}, Gender: #{giraffe.gender}"
-end
-=end
+Giraffe.create("Melvyn", 10, 18, 1600, "male")
+Giraffe.create("Gloria", 8, 16, 1200, "female")
+Giraffe.create("Rennie", 9, 17, 1400, "male")
+Giraffe.create("Judy", 10, 14, 1250, "female")
+Giraffe.create("Alice", 12, 16, 1450, "female")
+Giraffe.create("Sasuke", 9, 15, 1280, "male")
+Giraffe.create("John", 12, 16, 1450, "male")
+Giraffe.create("Cena", 9, 15, 1280, "male")
+
+puts Giraffe.all
